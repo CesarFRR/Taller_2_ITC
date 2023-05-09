6ee4737585d8677bfb7c38f1bce1b4b20fe37482
CesarFRR <crinconro@gmail.com>
2023-05-09 00:54:42 -0500

    Avance en clase AFD, Alfabeto y creacion de 2 txt

--- AFD.py
+++ AFD.py
@@ -1,3 +1,6 @@
+from Alfabeto import Alfabeto
+
+
 class AFD:
     Sigma=None
     Q=None
@@ -15,15 +18,39 @@ class AFD:
         
     def __init__(self, *args):
         if (len(args) == 1):  # Inicializar por archivo txt
-            pass
+            with open(args[0], 'r', newline=' ') as txt: #newline=' ' --> acepta todos los tipos de separadores de todos los sistemas operativos
+                lineas = txt.readlines()
+                [i.strip() for i in lineas] #elimina separadores (limpieza)
+                if(lineas[0] != "#!dfa"): raise Exception("Este archivo no es un AFD")
+                for i in range(1, len(lineas)):
+                    if(lineas[i] == "#alphabet"):
+                        i=i+1
+                        self.Sigma=Alfabeto(lineas[i])
+                    elif (lineas[i] =="#states"):
+                        while(lineas[i+1][0]=="s"):
+                            i=i+1
+                            self.Q.append(lineas[i])
+                    elif (lineas[i] =="#initial"):
+                        i=i+1
+                        self.q0=lineas[i]
+                    elif (lineas[i] =="#accepting"):
+                        while(lineas[i+1][0]=="s"):
+                            i=i+1
+                            self.F.append(lineas[i])
+                    elif (lineas[i] =="#transitions"):
+                        self.delta=[[None]*len(self.Sigma.simbolos) for i in range(len(self.Q))]
+                        print("ESTE ES DELTA:\n ",self.delta)
+                        while(lineas[i+1][0]=="s"):
+                            i=i+1
+                            # TODO: agregar delta de txt a la matriz self.delta --> MATRIZ: filas: q0, q1, q2, q3, q2,..,qn ; columnas: a, b, c,..,m-simbolo
+                        pass
         elif (len(args) == 5):  # Inicializar por los 5 parametros: alfabeto, estados, estadoInicial, estadosAceptacion, delta
             self.Sigma = args[0]
             self.Q = args[1]
             self.q0 = args[2]
             self.F = args[3]
             self.delta = args[4]
-        else:  # otras posibles sobrecargas de constructores (opcional)
-            pass
+
         
     def verificarCorregirCompletitudAFD():
         pass
--- Alfabeto.py
+++ Alfabeto.py
@@ -6,10 +6,32 @@
 # Ese arreglo se llamará simbolos[]
 #luego se usa ese arreglo para generar cadenas--> generarCadenaAleatoria(int n)
 
+import random
+
+
 class Alfabeto:
     simbolos = []
     def __init__(self, a_c):
-        pass
+        char = a_c.split("-")
+        self.min=char[0]
+        self.max=char[1]
+        if(self.min=="$" or self.max=="$"): raise ValueError("Rango de caracteres no valido, no se acepta lambda como uno de los rangos")
+        self.generarListaSimbolos(self.min, self.max)
+
+    def generarListaSimbolos(self, min, max):
+        for i in range (ord(min) , ord(max)+1):
+            self.simbolos.append(chr(i))
+        # print("min: ", self.min, "\nmax: ", self.max, "\nsimbolos: ", self.simbolos)
+        # print("min-num: ", ord(self.min), "\nmax-num: ", ord(self.max))
     
-    def generarCadenaAleatoria(n: int):
-        pass
+    def generarCadenaAleatoria(self, n: int):
+        string=""
+        for i in range (n):
+            string += self.simbolos[random.randint(0, len(self.simbolos)-1)]
+        return string
+
+# alf = Alfabeto("a-f")
+# print(alf.generarCadenaAleatoria(12))
+
+# alf2 = Alfabeto("0-3")
+# print(alf2.generarCadenaAleatoria(12))
