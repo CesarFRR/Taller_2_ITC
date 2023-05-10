3d3a3fa5bb78f22c77bcf5f6ed48d7ebfc7d10f4
Homeroso <82355484+Homeroso@users.noreply.github.com>
2023-05-09 20:47:53 -0500

    Funcion procesar AFD.py
    Funcion para procesar una cadena y retornar si es aceptada o no

--- AFD.py
+++ AFD.py
@@ -37,8 +37,23 @@ class AFD:
         pass
     def exportar(archivo):
         pass
-    def procesarCadena(cadena):
-        return True
+    def procesarCadena(cadena):   #Procesar cadena con delta como un diccionario
+        actual = self.q0
+        for i in cadena:
+            if i not in self.Sigma:  #Comprobar que el simbolo leido se encuentre en el alfabeto
+                return False
+            if self.delta.get(actual) is not None:  #Verificar que el estado actual exista
+                transicion = self.delta[actual]    #Lista de transiciones del estado actual
+                for j in transicion:    
+                    if i in j: #Recorrer las transiciones verificando el simbolo actual y el estado resultado
+                        actual = j[1] #Realizar transicion                    
+                        break
+                        
+        if actual in self.F: #verificar si el estado actual es de aceptacion
+            return True
+        else:
+            return False   
+    
     def procesarCadenaConDetalles(cadena):
         return True
     def procesarListaCadenas(listaCadenas,nombreArchivo, imprimirPantalla):
@@ -58,10 +73,9 @@ class AFD:
     def AFD_simplificarAFD(afdinput: AFD):
         pass
 
-print('a')
-
-    #se pudo?
-    #sirve? haga algo y dele ctr s, se guardo en mi pc, el punto blanco se fue, sirve , por cierto, instalese el tabNine, es una IA que le ayuda a codear mas rapido
-    #listo, aunque ya lo tenia pero se borro xd
-    # Tabnine AI, aaah ya, cierto, lo otro e que se puede hablar por meet, mmm, si eso seria todo, aaah, reunirnos a tal hora para abrir el live share y sale
-    #pero ya sabemos que funciona
+#Prueba
+x = AFD(['a','b'], ['q0','q1'],'q0',['q0'],{'q0':[['a','q1'],['b','q1']],'q1':[['a','q0'],['b','q0']]})#Automata que recibe cadenas con numero par de simbolos
+#Formato de delta: diccionario donde las keys son cada uno de los estados y los valores una matriz de los simbolos y la transicion de cada uno
+#Por ej: 'q0':[['a','q1'],['b','q1']] --> q0 es el estado, con a lleva a q1 y b a q2
+cadena = input('ingrese una cadena: ')
+print(x.procesarCadena(cadena))
