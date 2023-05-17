76f73136f3bae33c573deebea623cce1842413ba
CesarFRR <crinconro@unal.edu.co>
2023-05-17 05:18:50 -0500

    Mejoras en la lectura de archivos Co-authored-by: Gabriel Andres Anzola Tachak <gabriel.anzola@mercadolibre.com.co> Co-authored-by: Homeroso <Homeroso@users.noreply.github.com>

--- AFD.py
+++ AFD.py
@@ -1,99 +1,144 @@
 from Alfabeto import Alfabeto
+import re
 
 
 class AFD:
-    Sigma=None #se intentará hacer el delta como  un diccionario en esta rama, debido a la complejidad del taller
-    Q=None
-    q0=None
-    F=None
-    delta =[]
-    estadosLimbo=None
-    estadosInaccesibles=None
+    Sigma = None  # se intentará hacer el delta como  un diccionario en esta rama, debido a la complejidad del taller
+    Q = None
+    q0 = None
+    F = None
+    delta = {}
+    estadosLimbo = None
+    estadosInaccesibles = None
+    automata_tipo = "afd"
+    etiquetas=['#!dfa', '#alphabet', '#states', '#initial', '#accepting', '#transitions']
     # def __init__(self, alfabeto, estados, estadoInicial, estadosAceptacion, delta):
     #     self.Sigma = alfabeto
     #     self.Q = estados
     #     self.q0 = estadoInicial
     #     self.F = estadosAceptacion
     #     self.delta = delta
-        
+
     def __init__(self, *args):
         if (len(args) == 1):  # Inicializar por archivo txt
-            with open(args[0], 'r', newline=' ') as txt: #newline=' ' --> acepta todos los tipos de separadores de todos los sistemas operativos
-                lineas = txt.readlines()
-                [i.strip() for i in lineas] #elimina separadores (limpieza)
-                if(lineas[0] != "#!dfa"): raise Exception("Este archivo no es un AFD")
-                for i in range(1, len(lineas)):
-                    if(lineas[i] == "#alphabet"):
-                        i=i+1
-                        self.Sigma=Alfabeto(lineas[i])
-                    elif (lineas[i] =="#states"):
-                        while(lineas[i+1][0]!="#initial"):
-                            i=i+1
-                            self.Q.append(lineas[i])
-                       
-                        
-                    elif (lineas[i] =="#initial"):
-                        i=i+1
-                        self.q0=lineas[i]
-                    elif (lineas[i] =="#accepting"):
-                        while(lineas[i+1][0]!="#transitions"):
-                            i=i+1
-                            self.F.append(lineas[i])
-                    elif (lineas[i] =="#transitions"):
-                        # self.delta=[[None]*len(self.Sigma.simbolos) for i in range(len(self.Q))]
+            automata_tipo = "dfa"
+            if (not args[0].endswith("." + automata_tipo)):
+                raise ValueError(
+                    "El archivo proporcioando no es de formato ", automata_tipo)
+            try:
+                afc = {}
+                key = ''
+                with open(args[0], 'r', newline='', encoding='utf-8') as file:
+                    file = file.read().replace('\r\n', '\n').replace('\r', '\n')  # problema de saltos de linea solucionados
+                    string= f'''{file}'''
+                    if(not string.startswith(self.etiquetas[0])):
+                        print("si empieza con: ", self.etiquetas[0])
+                    dictReader={}
+                    for i in string.split('\n'):
+                        if i in self.etiquetas[1:]:
+                            key = i
+                        elif i != '':
+                            if(key!='#transitions' and Alfabeto.validate_regex(i,r"^[^#;\n]+$")):
+                                afc.setdefault(key, []).append(i)
+                            elif(key=='#transitions' and i.split(":")[1].split(">")[0]!="$"):
+                                trans=re.split(r"[:>]", i)
+                                #print("transition: ", trans, "\nLength: ", len(trans))
+                                if(len(trans)!=3): raise ValueError("transición inválida: ", i)
+                                afc.setdefault(key, []).append(i)
+                                print("\n trans: ", trans )
+                                if(trans[0] in dictReader and trans[2] in dictReader[trans[0]]): #si existe ese Q y ya existe la etiqueta evaluada...
+                                    raise ValueError("transición inválida --> AFD: no pueden haber dos transiciones de ", trans[0], " para la misma etiqueta ", trans[2], " --> ", i)
+                                elif(trans[0] in dictReader):
+                                    dictReader.setdefault(key, []).append(i)
+                                    print("\n trans: ", trans )
+                                    dictReader.setdefault(dictReader[trans[0]], {}).update({trans[1]:trans[2]}) #ej: para (q2, b)--> q5 ==> self.delta.setdefault("q2", {}).update({ "b": q5 })
+                                    #setdefault --> si no existe la clave "key" se crea una con valor [] --> "key":[]
+                    
+                    self.Sigma = Alfabeto([afc['#alphabet'][0]])
+                    self.Q = afc['#states']
+                    self.q0 = afc['#initial'][0]
+                    self.F = afc['#accepting']
+                    self.delta = dictReader
+                    print("dictreader: ", dictReader)
+            
+            except Exception as e:
+                print("Error en la lectura y procesamiento del archivo: ", e)
 
-                        self.delta=dict(zip(self.Q,[[None]*(len(self.Sigma.simbolos)) for i in range(len(self.Sigma.simbolos))])) #delta -> dict
-                        print("delta como diccionario:\n", self.delta)
-                        while(lineas[i+1][0]=="s"):
-                            i=i+1
-                            
-                            # TODO: agregar delta de txt a la matriz self.delta --> MATRIZ: filas: q0, q1, q2, q3, q2,..,qn ; columnas: a, b, c,..,m-simbolo
-                        pass
         elif (len(args) == 5):  # Inicializar por los 5 parametros: alfabeto, estados, estadoInicial, estadosAceptacion, delta
             self.Sigma = args[0]
             self.Q = args[1]
             self.q0 = args[2]
             self.F = args[3]
             self.delta = args[4]
+        # print("\n\n en orden:\n")
+        # print("alfabeto: ", self.Sigma.toStringEntrada())
+        # print("estados: ", self.Q)
+        # print("estado incial: ", self.q0)
+        # print("estados de aceptacion: ", self.F)
+        #print("transiciones (delta): ", self.delta)
+    def verificarCorregirCompletitudAFD(self):
+        pass
 
-        
-    def verificarCorregirCompletitudAFD():
+    def hallarEstadosLimbo(self):
         pass
-    def hallarEstadosLimbo():
+
+    def hallarEstadosInaccesibles(self):
         pass
-    def hallarEstadosInaccesibles():
+
+    def toString(self):
+        delta="d"
+        #print("dellta: ", self.delta)
+        for claveExterna, valorExterno in self.delta.items():
+            print(4)
+            for claveInterna, valorInterno in valorExterno.items():
+                print("v"+claveExterna.toString())
+                print(3)
+                delta=delta+str(claveExterna)+":"+str(claveExterna)+">"+str(valorInterno)
+        #print("asas--> ",delta)
+        #print("delta: ", type(delta), "Q: ", type(self.Q), "aceptacion: ", type(self.F), "delta: ", delta)
+        return ""#self.Sigma.formato_entrada+"\n".join(self.Q)+"\n"+self.q0+"\n".join(self.F)+delta
+
+    def imprimirAFDSimplificado(self):
         pass
-    def toString():
+
+    def exportar(self, archivo):
         pass
-    def imprimirAFDSimplificado():
-        pass
-    def exportar(archivo):
-        pass
-    def procesarCadena(cadena):
+
+    def procesarCadena(self, cadena):
         return True
-    def procesarCadenaConDetalles(cadena):
+
+    def procesarCadenaConDetalles(self, cadena):
         return True
-    def procesarListaCadenas(listaCadenas,nombreArchivo, imprimirPantalla):
-        pass
-    def AFD_hallarComplemento(afdInput):
-        pass
-    def AFD_hallarProductoCartesianoY(afd1: AFD,afd2: AFD):
-        pass
-    def AFD_hallarProductoCartesianoO(afd1: AFD, afd2: AFD):
-        pass
-    def AFD_hallarProductoCartesianoDiferencia(afd1: AFD, afd2: AFD):
-        pass
-    def AFD_hallarProductoCartesianoDiferenciaSimétrica(afd1: AFD, afd2: AFD):
-        pass
-    def AFD_hallarProductoCartesiano(afd1: AFD,afd2: AFD, StringOperacion):
-        pass
-    def AFD_simplificarAFD(afdinput: AFD):
+
+    def procesarListaCadenas(self, listaCadenas, nombreArchivo, imprimirPantalla):
         pass
 
-print('a')
+    def AFD_hallarComplemento(self, afdInput: "AFD"):
+        pass
 
-    #se pudo?
-    #sirve? haga algo y dele ctr s, se guardo en mi pc, el punto blanco se fue, sirve , por cierto, instalese el tabNine, es una IA que le ayuda a codear mas rapido
-    #listo, aunque ya lo tenia pero se borro xd
-    # Tabnine AI, aaah ya, cierto, lo otro e que se puede hablar por meet, mmm, si eso seria todo, aaah, reunirnos a tal hora para abrir el live share y sale
-    #pero ya sabemos que funciona
+    def AFD_hallarProductoCartesianoY(self, afd1: "AFD", afd2: "AFD"):
+        pass
+
+    def AFD_hallarProductoCartesianoO(self, afd1: "AFD", afd2: "AFD"):
+        pass
+
+    def AFD_hallarProductoCartesianoDiferencia(self, afd1: "AFD", afd2: "AFD"):
+        pass
+
+    def AFD_hallarProductoCartesianoDiferenciaSimétrica(self, afd1: "AFD", afd2: "AFD"):
+        pass
+
+    def AFD_hallarProductoCartesiano(self, afd1: "AFD", afd2: "AFD", StringOperacion):
+        pass
+
+    def AFD_simplificarAFD(self, afdinput: "AFD"):
+        pass
+
+print('Ejecutando:...')
+afd1= AFD("ej1.dfa")
+print(afd1.toString())
+# se pudo?
+# sirve? haga algo y dele ctr s, se guardo en mi pc, el punto blanco se fue, sirve , por cierto, instalese el tabNine, es una IA que le ayuda a codear mas rapido
+# listo, aunque ya lo tenia pero se borro xd
+# Tabnine AI, aaah ya, cierto, lo otro e que se puede hablar por meet, mmm, si eso seria todo, aaah, reunirnos a tal hora para abrir el live share y sale
+# pero ya sabemos que funciona
--- Alfabeto.py
+++ Alfabeto.py
@@ -1,37 +1,66 @@
-#Debido al formato de entrada, por ejemplo:
+# Debido al formato de entrada, por ejemplo:
 #         #alphabet
 #         a-c
 # Se debe usar un FOR para generar un arreglo de simbolos usando la numeracion ASCII
 # para ir desde (int) ASCII(a) hasta (int) ASCII(b) incluido
 # Ese arreglo se llamará simbolos[]
-#luego se usa ese arreglo para generar cadenas--> generarCadenaAleatoria(int n)
+# luego se usa ese arreglo para generar cadenas--> generarCadenaAleatoria(int n)
 
 import random
+import re
 
 
 class Alfabeto:
     simbolos = []
+    formato_entrada=None
+
     def __init__(self, a_c):
-        char = a_c.split("-")
-        self.min=char[0]
-        self.max=char[1]
-        if(self.min=="$" or self.max=="$"): raise ValueError("Rango de caracteres no valido, no se acepta lambda como uno de los rangos")
-        self.generarListaSimbolos(self.min, self.max)
+        self.formato_entrada=a_c
+        for i in a_c:
+            if ("$" in i):
+                raise ValueError(
+                    "Lambda no permitido como simbolo para el alfabeto")
+            elif (len(i) == 1):
+                self.simbolos.append(i[0])
+            elif (i[1] == "-"):
+                self.generarListaSimbolos(i[0], i[2])
+            else:
+                raise ValueError("Entrada de simbolos", i,
+                                 "no permitida para el alfabeto")
+        (self.simbolos).sort()
 
     def generarListaSimbolos(self, min, max):
-        for i in range (ord(min) , ord(max)+1):
+        for i in range(ord(min), ord(max)+1):
             self.simbolos.append(chr(i))
-        # print("min: ", self.min, "\nmax: ", self.max, "\nsimbolos: ", self.simbolos)
-        # print("min-num: ", ord(self.min), "\nmax-num: ", ord(self.max))
-    
+
     def generarCadenaAleatoria(self, n: int):
-        string=""
-        for i in range (n):
+        string = ""
+        for i in range(n):
             string += self.simbolos[random.randint(0, len(self.simbolos)-1)]
         return string
 
-# alf = Alfabeto("a-f")
-# print(alf.generarCadenaAleatoria(12))
+    @staticmethod
+    def validate_regex(cadena, expresion_regular) -> bool:
+        return (re.match(expresion_regular, cadena))
 
-# alf2 = Alfabeto("0-3")
-# print(alf2.generarCadenaAleatoria(12))
+    def toStringEntrada(self) -> str:
+        S=""
+        for i in self.formato_entrada:
+            if(self.formato_entrada.index(i)==0):
+                S=S+i
+            else:
+                S=S+"\n"+i
+        return S
+    def prueba (self, otro_alfabeto: "Alfabeto"):
+        pass
+
+# ejemplo = ["z", "0-5", "a", "b", "p", "k-o"]
+# alf1 = Alfabeto(ejemplo)
+# #print(alf1.simbolos)
+
+# print(alf1.toStringEntrada())
+# # alf = Alfabeto("a-f")
+# # print(alf.generarCadenaAleatoria(12))
+
+# # alf2 = Alfabeto("0-3")
+# # print(alf2.generarCadenaAleatoria(12))
