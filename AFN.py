50052f20c651e04d2ae97da729a4c8473ada57f7
Juan Montoya <juanjmontoya14@gmail.com>
2023-05-24 15:56:33 -0500

    toString, halllarEstadosInaccesibles

--- AFN.py
+++ AFN.py
@@ -25,11 +25,66 @@ class AFN:
         else:  # otras posibles sobrecargas de constructores (opcional)
             pass
 
-    def hallarEstadosInaccesibles():
-        pass
+    def validar_conexion(self, nodo_inicial, nodo_final):
+        visitados = set()  # Conjunto para almacenar los nodos visitados
+        pila = [nodo_inicial]  # Pila para almacenar los nodos a explorar
 
-    def toString():
-        pass
+        while pila:
+            nodo_actual = pila.pop()
+
+            if nodo_actual == nodo_final:
+                return True  # Se encontró una conexión entre los nodos
+
+            visitados.add(nodo_actual)
+
+            if nodo_actual in self.delta:
+                conexiones = self.delta[nodo_actual]  # Obtener las conexiones del nodo actual en el grafo
+                for simbolo, destinos in conexiones.items():
+                    for siguiente_nodo in destinos:
+                        if siguiente_nodo not in visitados:
+                            pila.append(siguiente_nodo)
+
+        return False  # No se encontró una conexión entre los nodos
+
+    def hallarEstadosInaccesibles(self):
+        estadosInaccesibles = []
+        for state in self.Q:
+            if not self.validar_conexion(self.q0, state):
+                estadosInaccesibles.append(state)
+        self.estadosInaccesibles = estadosInaccesibles
+        return estadosInaccesibles
+
+    def toString(self):
+        resultado = ""
+
+        resultado += "#!nfa\n"
+
+        # Agregar #alphabet
+        resultado += "#alphabet\n"
+        resultado += "-".join(self.Sigma) + "\n"
+
+        # Agregar #states
+        resultado += "#states\n"
+        for state in self.Q:
+            resultado += state + "\n"
+
+        # Agregar #initial
+        resultado += "#initial\n"
+        resultado += self.q0 + "\n"
+
+        # Agregar #accepting
+        resultado += "#accepting\n"
+        for state in self.F:
+            resultado += state + "\n"
+        resultado += "#transitions\n"
+        transitions = self.delta
+        for state in transitions:
+            for symbol in transitions[state]:
+                targets = transitions[state][symbol]
+                targets_str = ";".join(targets)
+                resultado += f"{state}:{symbol}>{targets_str}\n"
+
+        return resultado
 
     def imprimirAFNSimplificado():
         pass
@@ -37,7 +92,7 @@ class AFN:
     def exportar(archivo):
         pass
 
-    def AFD_AFNtoAFD(afn: AFN):
+    def AFD_AFNtoAFD(afn):
         pass
 
     def procesarCadena(cadena):
@@ -64,10 +119,14 @@ class AFN:
 
 
 
-print('a')
 
-# se pudo?
-# sirve? haga algo y dele ctr s, se guardo en mi pc, el punto blanco se fue, sirve , por cierto, instalese el tabNine, es una IA que le ayuda a codear mas rapido
-# listo, aunque ya lo tenia pero se borro xd
-# Tabnine AI, aaah ya, cierto, lo otro e que se puede hablar por meet, mmm, si eso seria todo, aaah, reunirnos a tal hora para abrir el live share y sale
-# pero ya sabemos que funciona
+M = AFN(
+        ['a','b'],      #Alfabeto de la Cinta
+        ['q0','q1'],    #Estados Internos   
+        'q0',           #Estado Inicial
+        ['q1'],         #Estados finales o de aceptacion
+        {'q0': {'a': ['q1'], 'b': ['q0', 'q1']}} # Funcion de Trancision
+    )
+
+print (f"Automata M:\n{str(M.toString())}")
+print (f"Estados Innacesibles: {str(M.hallarEstadosInaccesibles())}")
