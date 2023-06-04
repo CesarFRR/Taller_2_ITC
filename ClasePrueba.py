260bc87ab8d53a0d4758675a95122f8934c6ab44
Homeroso(Johan Rodriguez) <johrodriguezgu@unal.edu.co>
2023-06-04 16:04:17 -0500

    clasePrueba primer avance

--- ClasePrueba.py
+++ ClasePrueba.py
@@ -1,15 +1,79 @@
+import sys
+from Alfabeto import Alfabeto
+from AFD import AFD
 class ClasePrueba:
     def __init__(self, *args):
         pass
 
     def probarAFD(self):
-        pass
+        #Crear AFD
+        alfabeto = (str(input("Ingrese el alfabeto del AFD (ej: a-c, 0-9):\n")))
+        alfabeto = alfabeto.split(",")
+        alfabeto = Alfabeto(alfabeto)   #Generar alfabeto
+        
+        nroEstados = int(input("Ingrese el numero de estados del AFD: \n"))
+        estados = list()
+        print("Ingrese los estados del AFD:")
+        for i in range(nroEstados):
+            estados.append(input())   #Agregar estados ingresados
+        estados = set(sorted(estados))
+        print(estados)
+        
+        inicial = input("Ingrese el estado inicial: \n")
+        if inicial not in estados:  #Verificar que el estado inicial se encuentra en los estados ingresados
+            raise ValueError("El estado inicial no se encuentra en la lista de estados")
+        
+        nroAceptacion = int(input("Ingrese el numero de estados de aceptacion: \n"))
+        print("Ingrese los estados de aceptacion:")
+        aceptacion = set()
+        for i in range(nroAceptacion):
+            inp = input()
+            if inp in estados:
+                aceptacion.add(inp)
+            else:
+                raise ValueError("El estado de aceptacion "+aceptacion+ " no se encuentra en la lista de estados")
+        
+        transiciones = dict()
+        print("Ingrese las transiciones del AFD:")
+        for estado in estados:
+            transiciones[estado] = dict()
+            for simbolo in alfabeto.simbolos:       #Ingresar transicion de cada estado para cada uno de los simbolos
+                transicion = input("Ingrese transicion de "+estado+" con "+simbolo+":\n")
+                if transicion == "No":              #Ingresar "No" si no hay transicion
+                    next
+                elif transicion not in estados:
+                    raise ValueError("La transicion de "+estado+" con "+simbolo+" no se encuentra en la lista de estados")
+                else:
+                    transicion = list(transicion)
+                    transicion = "".join(transicion)
+                    transiciones[estado][simbolo] = transicion
+        
+        AFDCreado = AFD(alfabeto, estados, inicial, aceptacion, transiciones)
+        #print(AFDCreado.procesarCadenaConDetalles('aaabbb'))
+        
     def probarAFN(self):
         pass
     def probarAFNLambda(self):
         pass
     def main(self): # invoca a los otros para que puedan ser comentados facilmente y poder escoger cual se va a probar.
-        pass
+        opcion = input(
+            "--------------------------------------- \n"
+            "Seleccione metodo a probar \n"
+            "1. AFD \n"
+            "2. AFN \n"
+            "3. AFN Lambda \n"
+            "4. Salir \n"
+            "--------------------------------------- \n"
+            )
+        
+        if opcion == "1":
+            self.probarAFD()
+        elif opcion == "2":
+            self.probarAFN()
+        elif opcion == "3":
+            self.probarAFNLambda()
+        elif opcion == "4":
+            sys.exit()
 
     def probarAFNtoAFD(self):
         pass
@@ -23,4 +87,7 @@ class ClasePrueba:
         pass
     def probarSimplificacion(self):
         pass
-    
+
+Start = ClasePrueba()
+
+Start.main()
--- ProcesamientoCadenaAFN.py
+++ ProcesamientoCadenaAFN.py
@@ -1,7 +1,42 @@
+from AFN import nfa1
+import sys
+
 class ProcesamientoCadenaAFN:
-    cadena=None
-    esAceptada=None
-    listaProcesamientosAbortados=None
-    listaProcesamientosAceptacion=None
-    listaProcesamientosRechazados=None
-    
+    cadena = None
+    esAceptada = None
+    listaProcesamientosAbortados = None
+    listaProcesamientosAceptacion = None
+    listaProcesamientosRechazados = None
+    procesamiento = True
+    def __init__(self):
+        self.cadena = input("Ingrese una cadena:")
+
+    def elegirMetodo(self):
+        opcion = input(
+            "--------------------------------------- \n"
+            "Elija el metodo de procesamiento: \n"
+            "1. Procesamiento \n"
+            "2. Procesamiento con detalles \n"
+            "3. Computar Todos los procesamientos \n"
+            "0. Salir \n"
+            "--------------------------------------- \n"
+        )
+        if opcion == "1":
+            esAceptada = nfa1.procesarCadena(self.cadena)
+            return print(esAceptada)
+
+        elif opcion == "2":
+            esAceptada = nfa1.procesarCadenaConDetalles(self.cadena)
+            return print(esAceptada)
+
+        elif opcion == "3":
+            procesamientos = nfa1.computarTodosLosProcesamientos(self.cadena, "procesamientoAFN.txt")
+            self.listaProcesamientosAbortados = nfa1.abortadas
+            self.listaProcesamientosAceptacion = nfa1.aceptacion
+            self.listaProcesamientosRechazados = nfa1.rechazadas
+            return print(procesamientos)
+
+        elif opcion == "0":
+            sys.exit()
+
+
